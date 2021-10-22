b9e8ff03fee6101ade48104af683495e5e8e6a4e
drizzy <drizzy@webtrace.com>
2021-10-22 17:24:22 -0400

    Initial Commit

---
+++ template/templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  templateApp.swift
+//  template
+//
+//  Created by Macbook on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
